01136db3b1d942707679e68393b87ca91219164d
Marwan-Refaat <71664900+Marwan-Refaat@users.noreply.github.com>
2022-09-21 01:55:39 +0200

    Mixed up file names

--- create3_ws/src/scripts/create3ButtonLight.py
+++ create3_ws/src/scripts/create3ButtonLight.py
@@ -1,62 +1,65 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
-from irobot_create_msgs.msg import IrIntensityVector, LightringLeds, LedColor
+from rclpy.action import ActionClient
+from irobot_create_msgs.msg import InterfaceButtons, LightringLeds, LedColor
+from irobot_create_msgs.action import LedAnimation
 from rclpy.qos import ReliabilityPolicy, QoSProfile
 
-class lightController(Node):
+class animationController(Node):
 
-    def __init__(self):
-        super().__init__("lightController")
-        
-        #Subscribe to the ir_intensity topic, which has a message with type IrIntensityVector
-        self.irSubscriber = self.create_subscription(IrIntensityVector,"ir_intensity",self.ir_callback,QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
-        #Publish to the cmd_lightring topic, which uses messages with type LightringLeds
-        self.lightringPublisher  = self.create_publisher(LightringLeds,"cmd_lightring",10)
-        
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        
-        #Define the ir_readings variable to store readings
-        self.ir_readings = []
+   def __init__(self):
+       super().__init__("animationController")
+       
+       #Subscribe to the interface_buttons topic
+       self.buttonSubscriber = self.create_subscription(InterfaceButtons,"interface_buttons",self.button_callback,QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+       #Create an action client that sends an action of type LedAnimation to the action server led_animation
+       self.action_client = ActionClient(self, LedAnimation, 'led_animation')
 
 
-    def timer_callback(self):
-        #Initilaize message to correct message type
-        msg = LightringLeds()
-        msg.override_system = True #To override the default lightring settings
+   def send_goal(self):
+   	#Initialize an empty LedAnimation action tyoe
+       animationGoal = LedAnimation.Goal()
+       
+       #Animation Type (1 for blinking, 2 for spinning)
+       animationGoal.animation_type = 1
+       
+       #Runtime for the animation
+       animationGoal.max_runtime.sec = 10
+   	
+       #Initialize the lightring message
+       animationGoal.lightring = LightringLeds()
+       animationGoal.lightring.override_system = True
 
-        #Defining some LED colors to use later using the LedColor message type
-        blueLed = LedColor(red=0,green=0,blue=255)
-        redLed = LedColor(red=255,green=0,blue=0)
-        greenLed = LedColor(red=0,green=255,blue=0)
-        offLed = LedColor()
+       #Defining some LED colors to use later using the LedColor message type
+       blueLed = LedColor(red=0,green=0,blue=255)
+       redLed = LedColor(red=255,green=0,blue=0)
+       greenLed = LedColor(red=0,green=255,blue=0)
+       offLed = LedColor()
+       
+       #Setting the lightring colors
+       animationGoal.lightring.leds = [blueLed,blueLed,blueLed,blueLed,blueLed,blueLed]        
+   	
+       #Wait for an action server to become available
+       self.action_client.wait_for_server()
+       
+       #Send the goal
+       return self.action_client.send_goal_async(animationGoal)
+   
 
-        #Main Logic
-        if self.ir_readings: #Check if a valid reading exists
-			#! Write your code here!
-            #You can delete the example below and replace it with your logic.
-            
-            #! Example
-            #If the left proximity sensor detects an object    
-            if self.ir_readings[0].value >100: 
-            	#Make all 6 LEDs blue
-                msg.leds = [blueLed,blueLed,blueLed,blueLed,blueLed,blueLed]
-            
-            
-        self.lightringPublisher.publish(msg)
-
-        print("Publishing...")
-    
-    def ir_callback(self,msg):
-        self.ir_readings = msg.readings
+   
+   def button_callback(self,msg):
+       #! Write your code here!
+       #Hint: To check if a button is pressed, use the is_pressed property 
+       #Hint: To send a goal use the send_goal method created above
+       
+       
 
 def main():
-    rclpy.init()
+   rclpy.init()
 
-    controller = lightController()
+   controller = animationController()
 
-    rclpy.spin(controller)
+   rclpy.spin(controller)
 
 if __name__ == '__main__':
-    main()
+   main()
